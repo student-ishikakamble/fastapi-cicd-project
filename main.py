c84b25e5d201a07cba524f08bf3b2a0076377e1e
student-ishikakamble <ishikakamble24@gmail.com>
2026-06-29 20:17:54 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "CI/CD FastAPI project running 🚀"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
